77d8ca79ad19905b89253c7494f15cd4f5167f77
Cristian Luciano Custodio <cristian.custodio@benner.com.br>
2025-06-20 17:16:44 -0300

    Supabase configuração

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Web;
+using Web.Configuration;
+using Web.Services;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -10,5 +12,9 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddMudServices();
+var supabaseOptions = builder.Configuration
+    .GetSection(Web.Configuration.SupabaseOptions.SectionName)
+    .Get<Web.Configuration.SupabaseOptions>();
+builder.Services.AddScoped(sp => new Web.Services.SupabaseService(supabaseOptions));
 
 await builder.Build().RunAsync();
